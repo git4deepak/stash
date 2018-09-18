42ff6f93830cb9f016c12d86fd2ea80729a546cf
Deepak Soni <Deepak_Soni@notes.teradyne.com>
2018-09-18 12:48:18 -0400

    Added O(n) solution to max sum sub-array problem.

---
+++ MaxSumSubArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaxSumSubArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solution obj = new Solution();
+            int[] input = {-1,4,-6,1,1,1,2,-1,-1};
+            Console.WriteLine("Max Sum: {0}", obj.maxSubArray(input));
+            int[] input2 = { -1, -4, -6, -1, -1, -1, -2, -1, -1 };
+            Console.WriteLine("Max Sum: {0}", obj.maxSubArray(input2));
+        }
+    }
+}
---
+++ MaxSumSubArray/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxSumSubArray
+{
+    class Solution
+    {
+        public int maxSubArray(int[] nums)
+        {
+
+            int max_curr, max_global;
+            max_curr = max_global = nums[0];
+            int start, end, gStart, gEnd;
+            start = end = gStart = gEnd = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i] + max_curr)
+                {
+                    max_curr = nums[i];
+                    start = end = i;
+                }
+                else
+                {
+                    max_curr = nums[i] + max_curr;
+                    end = i;
+                }
+
+                //max_curr = Math.Max(nums[i], nums[i] + max_curr);
+
+                if (max_global < max_curr)
+                {
+                    max_global = max_curr;
+                    gStart = start;
+                    gEnd = end;
+                }
+            }
+            Console.WriteLine("Range [{0}, {1}]", gStart, gEnd);
+            return max_global;
+        }
+    };
+}
